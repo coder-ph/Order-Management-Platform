4036cfb054685bbeccebed380e2e87c38d13cb8f
Phelix <phelixmbani@gmail.com>
2025-02-20 00:45:01 +0300

    develop routes per user roles

--- Front End/client/src/App.jsx
+++ Front End/client/src/App.jsx
@@ -1,18 +1,53 @@
 import { useContext } from "react"
 import React from "react"
-import {BrowserRouter, Route, Routes, Navigate} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom'
 import {useSelector} from 'react-redux'
 import { selectRole, selectisAuthenticated } from "./Redux/Auth/authsSelectors"
 import Login from './Pages/LoginPage'
-
+import AdminDashboard from "./Components/AdminDashboard"
+import UserDashboard from "./Components/UserDashboard"
+import DriverDashboard from "./Components/DriverDashboard"
 
 function App() {
   
-  
+  const isAuthenticated = useSelector(selectisAuthenticated);
+  const role = useSelector(selectRole);
   return (
-    <>
-   
-    </>
+  
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />}/>
+
+        {isAuthenticated ? (
+          <>
+          {role === 'Admin' && (
+            <Route path="/admin/*" element={<AdminDashboard/>}/>
+          )}
+          {role === 'user' && (
+            <Route  path="/user/*" element= {<UserDashboard />}/>
+          )}
+          {role === 'driver' &&(
+            <Route path="/driver/*" element={<DriverDashboard/>}/>
+          )}
+          <Route path="*"
+          element = {
+            role === 'admin' ? (
+              <Navigate to= '/admin'/>
+            ) : role === 'user' ? (
+              <Navigate to='/user' />
+            ) : role === 'driver' ? (
+              <Navigate to='/driver' />
+            ) : (
+              <Navigate to= "/login" />
+            )
+          }
+          />
+          </>
+        ): (
+          <Route  path="*" element = {<Navigate to="/login" />}/>
+        )}
+      </Routes>
+    </Router>
   )
 }
 
---
+++ Front End/client/src/Components/DriverDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DriverDashboard() {
+  return (
+    <div>
+      Driver Dashboard
+    </div>
+  )
+}
+
+export default DriverDashboard
