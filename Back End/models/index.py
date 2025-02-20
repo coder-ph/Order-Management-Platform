19f7450379c39a037a090534f3e5522a58a2e6e3
pkamanthe <patriciamumbua97@gmail.com>
2025-02-20 21:16:19 +0300

    initial commit

--- Back End/models/index.py
+++
@@ -1,3 +0,0 @@
-# from location_model import loca
-from orders_model import *
-from store_model import *
--- Back End/models/store_model.py
+++ Back End/models/store_model.py
@@ -1,14 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
-from client import db
 from sqlalchemy import ForeignKey
+from client import db  
 
 class Store(db.Model):
     __tablename__ = 'stores'
     
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    storename = db.Column(db.String, nullable=False, unique=True)
+    storename = db.Column(db.String(100), nullable=False, unique=True) 
     user_id = db.Column(db.Integer, ForeignKey('users.id', ondelete="CASCADE"), nullable=False)
-    location_id = db.Column(db.Integer, ForeignKey('locations.id', ondelete="SET NULL"))
+    location_id = db.Column(db.Integer, ForeignKey('locations.id', ondelete="SET NULL"), nullable=True)
 
     owner = relationship("User", back_populates="stores")
     location = relationship("Location", back_populates="store")
+
+    def __repr__(self):
+        return f"Store('{self.storename}', Owner ID: {self.user_id}, Location ID: {self.location_id})"
