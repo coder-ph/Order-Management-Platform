c77a02e73b34ca2988eed8df044fe641f509b223
ken sofware engineer <mainakenken188@gmail.com>
2025-02-21 11:07:54 +0300

    created a store route and a simple psudo code

--- Back End/models/users_model.py
+++ Back End/models/users_model.py
@@ -15,7 +15,8 @@ class User(db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    phonemo = db.Column(db.String(15), nullable=True)  
+    phonemo = db.Column(db.String(15), nullable=True) 
+    role = db.Column(db.String(70)) 
     email = db.Column(db.String(100), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
 
@@ -24,6 +25,8 @@ class User(db.Model):
 
     def check_password(self, password):
         return bcrypt.check_password_hash(self.password_hash, password)
+    
+    
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
