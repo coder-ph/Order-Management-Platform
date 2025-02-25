256a614e400794b45af6e2229aa471885ea8164d
ken sofware engineer <mainakenken188@gmail.com>
2025-02-25 10:30:23 +0300

    committed order management

--- Back End/src/routers/store_router.py
+++ Back End/src/routers/store_router.py
@@ -5,21 +5,8 @@ from src.startup.logging import Logger
 
 logger = Logger("store routes file")
 
-class StoreTest(Resource):
-    def get(self):
-        return userTest()
-
-class Store(Resource):
-    def get(self):
 
 
-        ## run a store function
-        # check if there is an existing store details
-        # return the store details
-
-        
-        return Store()
-    
 
 
         
