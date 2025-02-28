aaf4ba7b2fde805876ad739992c28117e7dcbcd6
Debrah Navajjah Muinde <debbynav645@gmail.com>
2025-02-28 18:42:06 +0300

    Creates ManageOrders file

--- Front End/client/src/App.jsx
+++ Front End/client/src/App.jsx
@@ -24,6 +24,7 @@ import Ap from "./scenes/dashboard/App"
 import AdDashboard from "./scenes/dashboard/AdDashboard"
 import CategorySection from "./Components/CategorySection"
 import './App.css'
+import Orders from "./scenes/ManageOrders/ManageOrders"
 
 
 function App() {
