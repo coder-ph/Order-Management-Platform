ea46b572d4986bf76ad9e022ad5009aa59963fe6
Debrah Navajjah Muinde <debbynav645@gmail.com>
2025-03-11 20:15:47 +0300

    Fixes The Navbar css

--- Front End/client/src/Components/NavBar.jsx
+++ Front End/client/src/Components/NavBar.jsx
@@ -1,10 +1,15 @@
+import { useState } from 'react';
 import React from "react";
 import '../assets/styles/LPNavBar.css'
-
+import { RxHamburgerMenu } from "react-icons/rx";
 function Navbar() {
+    const [isOpen, setIsOpen] = useState(false)
     return (
         <nav className="nav-bar-lp">
-            <ul className="nav-list-lp">
+            <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+                <RxHamburgerMenu />
+                </div>
+            <ul className={isOpen ? "nav-list-lp open" : "nav-list-lp"}>
                 <li><a href="#home">HOME</a></li>
                 <li><a href="#about">ABOUT</a></li>
                 <li><a href="#services">SERVICES</a></li>
