0d42395e2435db6a9abe8d3e318629aa71ec5e79
Edmund <edmundgachanja@gmail.com>
2025-05-11 17:23:13 +0300

    dev

--- Front End/client/src/scenes/CompanyAnalytics/CompanyAnalytics.jsx
+++ Front End/client/src/scenes/CompanyAnalytics/CompanyAnalytics.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import ProfitRevenueRates from "../DriverManager/ProfitRevenueRates";
 
 const CompanyAnalytics = () => {
   return (
     <>
       <h1>This is Company Analyitics</h1>
+      <ProfitRevenueRates/>
 
      
     </>
